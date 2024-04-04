4d6e1031ce7a90ad7b0fb51ca969ead5164fc351
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-04 15:39:52 -0400

    Add files via upload

---
+++ Rename/Nagging-1/script.js
@@ -0,0 +1,6 @@
+document.getElementById('updateBtn').addEventListener('click', function() {
+    // Add logic to handle the update process
+    alert('Updating the app...');
+    window.open("home_new.html", "_self");
+});
+
