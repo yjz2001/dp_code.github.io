489e2ea46ff3640497b99d4e279dcaae0766a6b8
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-10 13:45:50 -0400

    Add files via upload

---
+++ DP_website/Test/Forced-2/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const updateRestartBtn = document.getElementById("update-restart");
+    const updateShutdownBtn = document.getElementById("update-shutdown");
+
+
+    updateRestartBtn.addEventListener("click", function() {
+        // Perform update and restart action
+        recordAction(1, "restart");
+        alert("Updating Windows and restarting...");
+        code_score_control(2);
+    });
+
+    updateShutdownBtn.addEventListener("click", function() {
+        // Perform update and shutdown action
+        alert("Updating Windows and shutting down...");
+        recordAction(1, "shutdown");
+        code_score_control(2);
+    });
+
+    
+
+});
+
