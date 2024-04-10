5612b70920c26f7a49a7b8ca6cfd08e2fe6c2b7c
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-10 13:40:30 -0400

    Add files via upload

---
+++ DP_website/Consequence/introduction/script.js
@@ -0,0 +1,17 @@
+function backBtnhome(x){
+    if(x === 1){
+        window.open("../Nagging-2/ex.html", "_self");
+    }
+    else if(x === 3){
+        window.open("../Sneaking-1/ex.html", "_self");
+    }
+    else if(x === 2){
+        window.open("../Obstruction-1/ex.html","_self");
+    }
+    else if(x === 4){
+        window.open("../Interference-3/ex.html", "_self");
+    }
+    else if(x === 5){
+        window.open("../Forced-1/ex.html", "_self");
+    }
+}
