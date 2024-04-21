6a6783e3f9ca425e812217de33c45464ad2093a1
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-21 17:04:42 -0400

    Add files via upload

---
+++ DP_website/Test/Nagging-1/script.js
@@ -0,0 +1,15 @@
+document.getElementById('updateBtn').addEventListener('click', function() {
+    // Add logic to handle the update process
+    alert('Updating the app...');
+    code_score_control(2);
+    window.open("../../Try/Nagging/trans_test_2.html", "_self");
+});
+
+document.getElementById('notNowBtn').addEventListener('click', function(){
+    window.open("home.html","_self");
+});
+
+function notNowClicked(){
+    //code_score_control(2);
+}
+
