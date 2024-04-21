29915f3910059915b7a17e7e4fc6a9b161aa4618
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-21 17:03:18 -0400

    Delete DP_website directory

--- DP_website/Test/Nagging-1/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('updateBtn').addEventListener('click', function() {
-    // Add logic to handle the update process
-    alert('Updating the app...');
-    window.open("home_new.html", "_self");
-});
-
--- DP_website/introduction/script.js
+++
@@ -1,3 +0,0 @@
-function backBtnhome(){
-    window.history.back()
-}
