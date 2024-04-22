0ae7d3277f4a163f745ee83f2a6ddb3102abaebc
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-04-22 14:25:30 -0400

    Add files via upload

---
+++ DP_website/Control_group/script.js
@@ -0,0 +1,17 @@
+function backBtnhome(x){
+    if(x === 1){
+        window.open("../Cg_Test/Nagging/trans_test_intro.html", "_self");
+    }
+    else if(x === 2){
+        window.open("../Cg_Test/Obstruction/trans_test_intro.html", "_self");
+    }
+    else if(x === 3){
+        window.open("../Cg_Test/Sneaking/trans_test_intro.html", "_self");
+    }
+    else if(x === 4){
+        window.open("../Cg_Test/Interference/trans_test_intro.html", "_self");
+    }
+    else if(x === 5){
+        window.open("../Cg_Test/Forced/trans_test_intro.html", "_self");
+    }
+}
