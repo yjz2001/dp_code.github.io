8e832d097fc37a54d225c4e3c43ae97b99565f7a
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-05-07 10:50:32 -0400

    Add files via upload

---
+++ DP_website/CgTest/Forced-3/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const signUpButton = document.getElementById("signup-btn");
+
+    signUpButton.addEventListener('click', function(event){
+        event.preventDefault(); // Prevent default form submission
+        //alert("Sign Up successfully and will receive discount news.");
+        recordAction(1, "signup");
+        localStorage.setItem("forced_record", 1);
+        window.open("../../Control_home.html","_self");
+    });
+
+});
