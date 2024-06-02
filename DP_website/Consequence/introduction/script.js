b5cf8cd45393fca13b0c8de070dbb76d39b938df
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-06-02 13:28:20 +0800

    Add files via upload

---
+++ DP_website/Consequence/introduction/script.js
@@ -0,0 +1,35 @@
+function backBtnhome(x){
+    if(x === 'Nagging'){
+        window.open("../../Try/Nagging/trans_experiment_intro.html", "_self");
+    }
+    else if(x === 'Obstruction'){
+        window.open("../../Try/Obstruction/trans_experiment_intro.html", "_self");
+    }
+    else if(x === 'Sneaking'){
+        window.open("../../Try/Sneaking/trans_experiment_intro.html", "_self");
+    }
+    else if(x === 'Interference'){
+        window.open("../../Try/Interference/trans_experiment_intro.html", "_self");
+    }
+    else if(x === 'Forced'){
+        window.open("../../Try/Forced/trans_experiment_intro.html", "_self");
+    }
+}
+
+function gonext(x){
+    if(x === 'Nagging'){
+        window.open("Nagging-1.html", "_self");
+    }
+    else if(x === 'Obstruction'){
+        window.open("Obstruction-1.html", "_self");
+    }
+    else if(x === 'Sneaking'){
+        window.open("Sneaking-1.html", "_self");
+    }
+    else if(x === 'Interference'){
+        window.open("Interference-1.html", "_self");
+    }
+    else if(x === 'Forced'){
+        window.open("Forced-1.html", "_self");
+    }
+}
