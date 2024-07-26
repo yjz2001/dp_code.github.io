126067f6bb590c18e6c15f2588c272e58804c073
yjz2001 <150075480+yjz2001@users.noreply.github.com>
2024-07-26 21:30:56 +0800

    Update script.js

--- DP_website/Try/Interference/script.js
+++ DP_website/Try/Interference/script.js
@@ -8,8 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             //alert("Your personalized ads have been disabled.");
         }
+        recordAction(1, 'confirm');
         continue_next();
     });
 
+    checkBox.addEventListener('change', function(){
+       if (checkBox.checked) {
+                console.log('checked');
+                recordAction(2,'checkBox');
+                // 在这里可以执行复选框被选中时的操作
+            } else {
+                console.log('uncheck');
+                recordAction(3,'checkBox');
+                // 在这里可以执行复选框被取消选中时的操作
+            } 
+    });
     
 });
